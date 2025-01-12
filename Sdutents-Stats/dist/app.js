f7448b9d0278bdf90f512472c4c85fe304c73d35
gregoryb79 <gregoryb79@gmail.com>
2025-01-12 09:17:36 +0200

    student stat

---
+++ Sdutents-Stats/dist/app.js
@@ -0,0 +1,56 @@
+var StatType = 0;
+var Answer = "";
+while (!((StatType === 1) || (StatType === 2))) {
+    Answer = prompt("Student Statistics. Do you want '1' - Best Grade or '2' - Worst Grade?");
+    if (Answer === null) {
+        alert("Looks like you don't want any grade. Good Day!");
+        throw "Canceled by the user.";
+    }
+    StatType = Number(Answer);
+    if (!((StatType === 1) || (StatType === 2))) {
+        alert("ERROR - wrong input, it must be 1 or 2!");
+    }
+}
+var MinGrade = 100;
+var MaxGrade = 0;
+var StudentName = "";
+var NameAnswer = "";
+while (Answer !== null) {
+    Answer = prompt("Enter Student's Full Name. Cancel to stop.");
+    if (Answer === null) {
+        break;
+    }
+    NameAnswer = Answer;
+    var CurrGrade = -1;
+    while ((CurrGrade < 0) || (CurrGrade > 100)) {
+        Answer = prompt("Enter Student's grade. Cancel to stop.");
+        if (Answer === null) {
+            break;
+        }
+        CurrGrade = Number(Answer);
+        if (isNaN(CurrGrade)) {
+            alert("ERROR! This is not a number.");
+        }
+        else if ((CurrGrade < 0) || (CurrGrade > 100)) {
+            alert("The grade must be between 0 and 100.");
+        }
+    }
+    if (StatType === 1) {
+        if (CurrGrade > MaxGrade) {
+            MaxGrade = CurrGrade;
+            StudentName = NameAnswer;
+        }
+    }
+    else {
+        if (CurrGrade < MinGrade) {
+            MinGrade = CurrGrade;
+            StudentName = NameAnswer;
+        }
+    }
+}
+if (StatType === 1) {
+    alert("The best student is: " + StudentName);
+}
+else {
+    alert("The worst student is: " + StudentName);
+}
