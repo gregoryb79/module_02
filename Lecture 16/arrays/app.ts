dff7a38abbd8514b18a6cedb32deb48883fa5f36
gregoryb79 <gregoryb79@gmail.com>
2025-01-20 09:14:26 +0200

    sorted array

--- Lecture 16/arrays/app.ts
+++ Lecture 16/arrays/app.ts
@@ -1,4 +1,3 @@
-alert("Enter numbers");
 
 // function getNumber (numCount : number):number{
 
@@ -16,15 +15,76 @@ alert("Enter numbers");
         
 // }
 
+function numbersToArray() {
+    let numbers: number[] = [];
+    let userInput = prompt("enter number");
 
-let numbers: number[] = [];
-let userInput = prompt("enter number");
+    while (userInput !== null){
+        numbers.push(Number(userInput));
+        userInput = prompt("enter number");    
+    }
 
-while (userInput !== null){
-     numbers.push(Number(userInput));
-     userInput = prompt("enter number");    
+    for(let i = 0; i < numbers.length; i++){
+        alert(numbers[i]);
+    }
 }
 
-for(let i = 0; i < numbers.length; i++){
-    alert(numbers[i]);
+function stringsReader() {
+
+    let strings : string [] = [];
+    let userInput = prompt("enter string");
+
+    while (userInput !== null){
+        strings.push(userInput);
+        userInput = prompt("enter string");    
+    }
+
+    const index = Number(prompt("enter number"));
+
+    alert(`String number ${index} is ${strings[index-1]}.`);
+
 }
+
+function getArray(){
+    let numbers: number[] = [];
+    let userInput = prompt("enter number");
+    
+    while (userInput !== null) {
+        userInput = prompt("enter number"); 
+        numbers[numbers.length] = Number(userInput);
+    }
+
+    return numbers;
+
+}
+
+function bubbleSort(array: number[]): number[] {
+    const n = array.length;
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (array[j] > array[j + 1]) {
+          [array[j], array[j + 1]] = [array[j + 1], array[j]]; // Swap
+        }
+      }
+    }
+    return array;
+  }
+
+function sortedArray(){
+
+    let numbers: number[] = [];
+    let userInput = prompt("enter number");
+    
+    numbers = getArray();
+
+    numbers = bubbleSort(numbers);
+
+
+    alert(numbers);
+
+}
+
+//stringsReader();
+
+sortedArray();
+
--- Lecture 16/arrays/dist/app.js
+++ Lecture 16/arrays/dist/app.js
@@ -1,4 +1,3 @@
-alert("Enter numbers");
 // function getNumber (numCount : number):number{
 //     let UserInput = NaN;
 //     while (isNaN(UserInput)) {
@@ -9,12 +8,54 @@ alert("Enter numbers");
 //     }
 //     return UserInput;
 // }
-var numbers = [];
-var userInput = prompt("enter number");
-while (userInput !== null) {
-    numbers.push(Number(userInput));
-    userInput = prompt("enter number");
+function numbersToArray() {
+    var numbers = [];
+    var userInput = prompt("enter number");
+    while (userInput !== null) {
+        numbers.push(Number(userInput));
+        userInput = prompt("enter number");
+    }
+    for (var i = 0; i < numbers.length; i++) {
+        alert(numbers[i]);
+    }
 }
-for (var i = 0; i < numbers.length; i++) {
-    alert(numbers[i]);
+function stringsReader() {
+    var strings = [];
+    var userInput = prompt("enter string");
+    while (userInput !== null) {
+        strings.push(userInput);
+        userInput = prompt("enter string");
+    }
+    var index = Number(prompt("enter number"));
+    alert("String number " + index + " is " + strings[index - 1] + ".");
 }
+function getArray() {
+    var numbers = [];
+    var userInput = prompt("enter number");
+    while (userInput !== null) {
+        userInput = prompt("enter number");
+        numbers[numbers.length] = Number(userInput);
+    }
+    return numbers;
+}
+function bubbleSort(array) {
+    var _a;
+    var n = array.length;
+    for (var i = 0; i < n - 1; i++) {
+        for (var j = 0; j < n - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                _a = [array[j + 1], array[j]], array[j] = _a[0], array[j + 1] = _a[1]; // Swap
+            }
+        }
+    }
+    return array;
+}
+function sortedArray() {
+    var numbers = [];
+    var userInput = prompt("enter number");
+    numbers = getArray();
+    numbers = bubbleSort(numbers);
+    alert(numbers);
+}
+//stringsReader();
+sortedArray();
